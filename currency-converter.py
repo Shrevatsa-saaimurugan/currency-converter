335dc621ad23e4def82a6741d1612058c3cf1e77
robo <152064810+Shrevatsa-saaimurugan@users.noreply.github.com>
2024-05-05 20:08:34 +0530

    Add files via upload

---
+++ currency-converter.py
@@ -0,0 +1,14 @@
+a = int(input("dsfhggrth:"))
+b = int(82)
+c = int(108)
+d = int(32)
+e = input("which thpe of converter 1 is $ 2 is pound 3 is singpure")
+if 1 :
+   print(b *a)
+   print("your courency from $ has converted to rupies")
+elif 2 :
+   print(c*a)
+   print("your courency from pound has converted to rupies")
+elif 3 :
+   print(d *a)  
+   print("your courency from singapure $ has converted to rupies")
